6a97c40f4a8bd6b30ceec42e856635b84f87fc64
TJ <tj@TJs-MacBook-Pro.local>
2025-11-27 14:51:14 -0500

    Integrated Trademark Logo and updated static settings

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+# Serve static files during development
+if settings.DEBUG and settings.STATICFILES_DIRS:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
